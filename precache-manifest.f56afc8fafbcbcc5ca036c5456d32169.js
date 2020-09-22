8c36e2f78f2f93ffcf8f7ea347a2fc63c5f8c0a4
Harrison <fayvorharrison101@gmail.com>
2020-09-22 19:53:40 +0100

    Updates

---
+++ precache-manifest.f56afc8fafbcbcc5ca036c5456d32169.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c09138188e9e6740b3c8dd5f1ebad306",
+    "url": "/index.html"
+  },
+  {
+    "revision": "400684c02587a279492a",
+    "url": "/static/css/main.6705fb57.chunk.css"
+  },
+  {
+    "revision": "640fe445b255f1791575",
+    "url": "/static/js/2.3d99367e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.3d99367e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "400684c02587a279492a",
+    "url": "/static/js/main.539da520.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "61379a3d92baf8560ec0572eb7424fcc",
+    "url": "/static/media/img1.61379a3d.png"
+  },
+  {
+    "revision": "bed7cf4acea20e4a8dcffb2d4bc28d7f",
+    "url": "/static/media/img2.bed7cf4a.png"
+  }
+]);
